2ef61cedfc603f07f560d73b5baa6f226930090d
Alex <alexanderrosen45@gmail.com>
2023-05-03 13:07:10 -0400

    mse loss

--- main.cpp
+++ main.cpp
@@ -1,61 +1,23 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <cmath>
+#include <vector>
 using namespace std;
 
-class Node
-{
-private:
-    double data;
-
-public:
-    Node(double data)
-    {
-        this->data = data;
+double mse_loss(std::vector<double> y_true, std::vector<double> y_pred) {
+    double sum = 0.0;
+    for (int i = 0; i < y_true.size(); i++) {
+        sum += std::pow(y_true[i] - y_pred[i], 2);
     }
+    return sum / y_true.size();
+}
 
-    // return Node's data attribute
-    double getData() const
-    {
-        return data;
-    }
+int main() {
+    std::vector<double> y_true = {1, 0, 0, 1};
+    std::vector<double> y_pred = {0, 0, 0, 0};
 
-    // cast Node to string
-    operator std::string() const
-    {
-        std::string str = "Node(data=";
-        str += std::to_string(data);
-        str += ")";
-        return str;
-    }
+    std::cout << mse_loss(y_true, y_pred) << std::endl; // 0.5
 
-    // sum two Node's data
-    Node node_sum(const Node &node)
-    {
-        double new_data = data + node.getData();
-        Node new_node(new_data);
-        return new_node;
-    }
-
-    // multiply two Node's data
-    Node node_product(const Node &node)
-    {
-        double new_data = data * node.getData();
-        Node new_node(new_data);
-        return new_node;
-    }
-};
-
-int main(int argc, char *argv[])
-{
-    int data;
-
-    cout << "Enter node's 'data'(double): ";
-    cin >> data;
-
-    Node node1(data);
-    Node node2(data);
-    Node node = node1.node_product(node2);
-
-    std::cout << static_cast<std::string>(node) << std::endl;
+    return 0;
 }
